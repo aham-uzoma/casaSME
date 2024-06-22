d6318427627ca3037f8e389795ac0a6b98cd6bd4
aham-uzoma <ahamefula.uzoma@gmail.com>
2024-06-22 15:16:49 +0100

    edit pages

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3500';
+//const BASE_URL = 'http://localhost:3500';
 //const BASE_URL = 'https://casasmeapi.casasme.com.ng'
-//const BASE_URL = 'https://casasmeapi.onrender.com'
+const BASE_URL = 'https://casasmeapi.onrender.com'
 
 
 export default axios.create({
